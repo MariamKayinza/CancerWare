e3831be21fb543b762a92ab2f28c7a2406ad42cb
MariamKayinza <2020bcs036@std.must.ac.ug>
2023-07-19 10:44:50 +0300

    better design

---
+++ app/src/main/java/com/example/cancerware/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.example.cancerware;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, new HomeFragment())
+                        .commit();
+                return true;
+
+            case R.id.scan:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, new ScanFragment())
+                        .commit();
+                return true;
+
+            case R.id.location:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, new ClinicFragment())
+                        .commit();
+                return true;
+
+            case R.id.info:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.flFragment, new InfoFragment())
+                        .commit();
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+}
--- app/src/main/java/com/example/cancerware/StartScreen.java
+++ app/src/main/java/com/example/cancerware/StartScreen.java
@@ -11,32 +11,11 @@ import android.widget.Button;
 public class StartScreen extends AppCompatActivity {
 
     private ImageView imageView;
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
-
-        button = findViewById(R.id.button);
-        imageView = findViewById(R.id.imageView);
-
-        // Set the image for the splash screen
-        int imageResource = R.drawable.cancerware;
-        imageView.setImageResource(imageResource);
-
-        // Set the button text for the home page
-        String buttonText = "GET STARTED";
-        button.setText(buttonText);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // home
-                Intent intent = new Intent(StartScreen.this, homeActivity.class);
-                startActivity(intent);
-            }
-        });
 
         // Set a timer to delay the start of the main activity
         int SPLASH_TIME_OUT = 3000;
@@ -44,7 +23,7 @@ public class StartScreen extends AppCompatActivity {
             @Override
             public void run() {
                 // Start the main activity
-                Intent intent = new Intent(StartScreen.this, homeActivity.class);
+                Intent intent = new Intent(StartScreen.this, HomeActivity.class);
                 startActivity(intent);
 
                 // Finish the splash activity
